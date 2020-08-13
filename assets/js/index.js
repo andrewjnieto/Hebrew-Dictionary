4c10729c49e341a8de9f15c197b2c55dbe532b04
Andrew Nieto <andrewjnieto@gmail.com>
2020-08-13 16:53:48 -0400

    Fixed js files

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,7 +22,7 @@ function getWords(searchTerm) {
 }
 
 function outputWords(data) {
-	let output = "";
+	let output = "<h2 id = \"results-header\">Results</h2>";
 	for(let i = 0; i < data.length; i++) {
 		output += outputWord(i + 1, data[i]);
 	}
@@ -31,27 +31,24 @@ function outputWords(data) {
 
 function outputWord(index, def) {
 	let results = $("#result-container").get(0);
-	let result = common_tags.word_container +
-	common_tags.row_container +
-	common_tags.hebrew_word_container +
+	let result = '<div class="definition">' + 
 	formatHeader(index, def) +
-	formatDetails(def) +
 	formatAllDefs(def["all_defs"]) +
-	"</div> </div> </div>";
+	formatDetails(def) +
+	'</div>';
 	return result;
 }
 
 
 function formatHeader(index, def) {
 	let header = "<h3>";
-	header += `${index}) ${def.no_niqqud} • ${def.niqqud} -- ${def.kjv_def}`;
+	header += `${index}) ${def.no_niqqud} • ${def.niqqud} (${def.morph})`;
 	return header + "</h3>";
 }
 
 function formatDetails(def) {
-	return `<h4>morphology: (${def.morph})</h4>` +
-		   `<h4>Strong's Def: ${def.strongs_def}</h4>` +
-		   `<h4>King James Def: ${def.kjv_def}</h4>`;
+	return `<small>Strong's Def: ${def.strongs_def}</small> -- ` +
+		   `<small>King James Def: ${def.kjv_def}</small>`;
 }
 
 function formatAllDefs(all_defs) {
